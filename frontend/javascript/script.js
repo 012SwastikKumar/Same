2f1f71fe5883303734b449e940093863f5527c77
Swastik <swastiktovisit1234@gmil.com>
2022-03-02 17:48:48 +0530

    header_skeleton_irresponsive

---
+++ frontend/javascript/script.js
@@ -0,0 +1,16 @@
+brandBtn.addEventListener('click', () => {
+    window.location.pathname = ('../index.html')
+})
+navHomeBtn.addEventListener('click', () => {
+    window.location.pathname = ('../index.html')
+})
+navIssuesBtn.addEventListener('click', () => {
+    location.href = ('views/issues.html')
+})
+
+navStatusBtn.addEventListener('click', () => {
+    location.href = ('views/status.html')
+})
+navSolvedListBtn.addEventListener('click', () => {
+    location.href = ('views/solved_list.html')
+})
---
+++ frontend/javascript/variables.js
@@ -0,0 +1,8 @@
+// BRAND FOR HOME PAGE DIRECTLY
+const brandBtn = document.getElementById('brand');
+// NAV BUTTONS
+const navHomeBtn = document.getElementById('nav_home');
+const navIssuesBtn = document.getElementById('nav_issues');
+const navStatusBtn = document.getElementById('nav_status');
+const navSolvedListBtn = document.getElementById('nav_solved_list');
+// const activeBtn = document.getElementsByClassName('active');
